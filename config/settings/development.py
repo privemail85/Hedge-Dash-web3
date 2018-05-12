d848f1621f949ebf79bf51dde6083095f4512743
Nash <nashruddin.amin@gmail.com>
2018-05-12 15:09:43 +0700

    heroku issue

--- config/settings/development.py
+++ config/settings/development.py
@@ -12,6 +12,6 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(str(ROOT_DIR), 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -1,5 +1,4 @@
 import os
-
 import django_heroku
 from .base import *
 
@@ -7,19 +6,13 @@ from .base import *
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-ADMINS = ['nashruddin.amin@gmail.com']
 
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
     }
 }
-
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
-ANYMAIL = {
-    'MAILGUN_API_KEY': os.getenv('MAILGUN_API_KEY'),
-    'MAILGUN_SENDER_DOMAIN': os.getenv('MAILGUN_SENDER_DOMAIN'),
-}
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@example.com')
 
 django_heroku.settings(locals())
