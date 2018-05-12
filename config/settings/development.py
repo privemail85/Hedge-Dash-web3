cf86f3f7256c5392bbab005a358f99d6171dbd39
Nash <nashruddin.amin@gmail.com>
2018-05-12 15:12:01 +0700

    heroku issue

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,14 +1,12 @@
+import os
 from .base import *
 
 
 SECRET_KEY = '5pr8=^=iz-_ghb9ybc%6x1rlk!a@j5yu#^s&bg+y8s@ve&5v6k'
-
 DEBUG = True
-ALLOWED_HOSTS = []
 
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
