bfadcabe156e66cb83e5932a9624f626ba2dac97
Nash <nashruddin.amin@gmail.com>
2018-05-12 15:47:28 +0700

    heroku issue

--- apps/blueprints/views.py
+++ apps/blueprints/views.py
@@ -1,73 +1,65 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from web3 import Web3, HTTPProvider
 
+from .abi_defs import factoryAbi, mintAbi, factoryAddress, mintAddress, blueprintAbi
 
+mintAddress = "0xfD69413a26b451fdAA8b51FbeE1581752B9F745F"
+masterAddress = "0xA3A4435aE1e025E184FfC80763D30Ea55a8C9372"
+
+contractOwner = "0x002aF9F9CB3fE3416881fD63C183826C6e59ad45"
+
+infuraTestURL = 'https://ropsten.infura.io/Ke3AtZSlNpKtZEJz6JQR'
+
+HARDCODED_USER_WALLET = "0x106F9774c34773a3d1827ead1195d6B737e3f443"
 
 def index(request):
     return render(request, 'blueprints/index.html')
 
+def createablueprint(request):
+    return render(request, 'blueprints/createablueprint.html')
 
-# from web3 import Web3, HTTPProvider
+def instructions(request):
+    return render(request, 'blueprints/instructions.html')
 
-# from .abi_defs import factoryAbi, mintAbi, factoryAddress, mintAddress, blueprintAbi
+def myblueprints(request):
+    web3 = Web3(HTTPProvider(infuraTestURL))
+    mintContract = web3.eth.contract(address = mintAddress, abi = mintAbi)
+    factoryContract = web3.eth.contract(address = factoryAddress, abi = factoryAbi)
 
-# mintAddress = "0xfD69413a26b451fdAA8b51FbeE1581752B9F745F"
-# masterAddress = "0xA3A4435aE1e025E184FfC80763D30Ea55a8C9372"
+    blueprintList = []
 
-# contractOwner = "0x002aF9F9CB3fE3416881fD63C183826C6e59ad45"
+    noOfBlueprints = factoryContract.functions.noOfBlueprints().call()
 
-# infuraTestURL = 'https://ropsten.infura.io/Ke3AtZSlNpKtZEJz6JQR'
-
-# HARDCODED_USER_WALLET = "0x106F9774c34773a3d1827ead1195d6B737e3f443"
-
-# def index(request):
-#     return render(request, 'blueprints/index.html')
-
-# def createablueprint(request):
-#     return render(request, 'blueprints/createablueprint.html')
-
-# def instructions(request):
-#     return render(request, 'blueprints/instructions.html')
-
-# def myblueprints(request):
-#     web3 = Web3(HTTPProvider(infuraTestURL))
-#     mintContract = web3.eth.contract(address = mintAddress, abi = mintAbi)
-#     factoryContract = web3.eth.contract(address = factoryAddress, abi = factoryAbi)
-
-#     blueprintList = []
-
-#     noOfBlueprints = factoryContract.functions.noOfBlueprints().call()
-
-#     for n in range (1,noOfBlueprints+1):
+    for n in range (1,noOfBlueprints+1):
         
-#         address,ticker,margin,expirationTime,creationTime,creatorID,creatorAddress = factoryContract.functions.lookupBlueprintMeta(n).call()
+        address,ticker,margin,expirationTime,creationTime,creatorID,creatorAddress = factoryContract.functions.lookupBlueprintMeta(n).call()
 
-#         print(margin)
+        print(margin)
 
 
-#         if (address != "0x0000000000000000000000000000000000000000"):
-#             #blueprintAddress = blueprint[0]
-#             #blueprintContract = web3.eth.contract(address = blueprintAddress, abi = blueprintAbi)
+        if (address != "0x0000000000000000000000000000000000000000"):
+            #blueprintAddress = blueprint[0]
+            #blueprintContract = web3.eth.contract(address = blueprintAddress, abi = blueprintAbi)
 
-#             entryPrice = 0
-#             exitPrice = 0
+            entryPrice = 0
+            exitPrice = 0
 
-#             blueprintID = n
+            blueprintID = n
             
             
-#             blueprintList.append(blueprints(blueprintID,address,ticker,margin,expirationTime,creationTime,creatorID,creatorAddress,entryPrice,exitPrice))
+            blueprintList.append(blueprints(blueprintID,address,ticker,margin,expirationTime,creationTime,creatorID,creatorAddress,entryPrice,exitPrice))
 
-#     print (blueprintList) 
-#     #event_filter = factoryContract.eventFilter('BlueprintCreated')
-#     #print (event_filter.getFilterLogs())
+    print (blueprintList) 
+    #event_filter = factoryContract.eventFilter('BlueprintCreated')
+    #print (event_filter.getFilterLogs())
 
     
 
-#     return render(request, 'blueprints/myblueprints.html', {"blueprintList": blueprintList})
+    return render(request, 'blueprints/myblueprints.html', {"blueprintList": blueprintList})
 
-# def purchasedblueprints(request):
-#     return render(request, 'blueprints/purchasedblueprints.html')
+def purchasedblueprints(request):
+    return render(request, 'blueprints/purchasedblueprints.html')
 
-# def blueprintsubmissionconfirmation(request):
-#     return render(request, 'blueprints/blueprintsubmissionconfirmation.html')
-#     
+def blueprintsubmissionconfirmation(request):
+    return render(request, 'blueprints/blueprintsubmissionconfirmation.html')
+    
