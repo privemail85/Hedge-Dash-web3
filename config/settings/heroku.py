09f97dd94b1ff45ba3c56abe2563ca1fe46509ed
Nash <nashruddin.amin@gmail.com>
2018-05-12 14:09:01 +0700

    install django-anymail package

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -4,12 +4,19 @@ from .base import *
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {}
 }
+
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+ANYMAIL = {
+    'MAILGUN_API_KEY': os.getenv('MAILGUN_API_KEY'),
+    'MAILGUN_SENDER_DOMAIN': os.getenv('MAILGUN_SENDER_DOMAIN'),
+}
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL') or 'noreply@example.com'
 
 import django_heroku
 django_heroku.settings(locals())
